3c9d79d0994c38d406439ff9674cbeee367eff05
Taka Wang <wang.chia.ming@gmail.com>
2014-12-29 14:04:52 +0800

    Add ble scan scripts

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 bbb-beacon-led
 ==============
+sudo apt-get install bluez python-bluez
+pip install Adafruit_BBIO
---
+++ test.py
@@ -0,0 +1,41 @@
+from led import *
+import time
+import blescan
+import sys
+
+import bluetooth._bluetooth as bluez
+
+dev_id = 0
+try:
+	sock = bluez.hci_open_dev(dev_id)
+	print "ble thread started"
+
+except:
+	print "error accessing bluetooth device..."
+    	sys.exit(1)
+
+blescan.hci_le_set_scan_parameters(sock)
+blescan.hci_enable_le_scan(sock)
+
+while True:
+	led_all(False) # turn off
+	mac = ""
+	val = -200
+	returnedList = blescan.parse_events(sock, 10)
+	print "----------"
+	for beacon in returnedList:
+		#print beacon
+		result = beacon.split(",")
+		if int(result[5]) > val :
+			val = result[5]
+			mac = result[0]
+	if (mac == "7e:60:6f:3b:d9:47"):
+		led_a(True)
+		print("IPAD:" + result[5])
+	elif (mac == "d0:39:72:c3:a2:d6"):
+		led_b(True)
+		print("BLE MINI:" + result[5])
+	else:
+		print("Unknown " + result[0])
+	time.sleep(0.5)
+
