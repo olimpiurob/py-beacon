4f5529f449970795dbaff834a18924cd8526d0c8
Olimpiu Rob <olimpiu.rob@gmail.com>
2016-05-23 22:58:32 +0300

    Added more MQTT config options

--- emitter.py
+++ emitter.py
@@ -8,23 +8,42 @@ from proximity import *
 
 DEBUG = True
 calculator = 0
-conf  = 0
+conf = 0
+
 
 def onConnect(client, userdata, rc):
     """MQTT onConnect handler"""
     print("Connected to broker: " + str(rc))
     client.subscribe(conf["topic_id"] + "#")
 
+
 def onMessage(client, userdata, msg):
     """MQTT subscribe handler
     Push new beacon info to calculator for proximity calculation
     """
-    obj = json.loads(msg.payload) # decode json string
+    obj = json.loads(msg.payload)  # decode json string
     calculator.add(obj["id"], int(obj["val"]))
 
-def initMQTT(url = "localhost", port = 1883, keepalive = 60):
+
+def initMQTT(url="localhost", port=1883, keepalive=60, client_id=None,
+             username=None, password=None, certificate=None, client_key=None,
+             client_cert=None, mqtt_protocol="3.1.1"):
     """Init MQTT connection"""
-    client = mqtt.Client()
+    proto = mqtt.MQTTv311
+    if protocol == "3.1":
+        proto = mqtt.MQTTv31
+
+    if not client_id:
+        client = mqtt.Client(protocol=proto)
+    else:
+        client = mqtt.Client(client_id, protocol=proto)
+
+    if username is not None:
+        client.username_pw_set(username, password)
+
+    if certificate is not None:
+        client.tls_set(certificate, certfile=client_cert, keyfile=client_key)
+
     client.on_connect = onConnect
     client.on_message = onMessage
     try:
@@ -34,6 +53,7 @@ def initMQTT(url = "localhost", port = 1883, keepalive = 60):
     except Exception, e:
         print(e)
         return None
+
 
 def init():
     """Read config file"""
@@ -45,6 +65,13 @@ def init():
     ret["url"]           = config.get('MQTT', 'url')
     ret["port"]          = int(config.get('MQTT', 'port'))
     ret["keepalive"]     = int(config.get('MQTT', 'keepalive'))
+    ret["client_id"]     = config.get('MQTT', 'client_id')
+    ret["username"]      = config.get('MQTT', 'username')
+    ret["password"]      = config.get('MQTT', 'password')
+    ret["certificate"]   = config.get('MQTT', 'certificate')
+    ret["client_key"]    = config.get('MQTT', 'client_key')
+    ret["client_cert"]   = config.get('MQTT', 'client_cert')
+    ret["mqtt_protocol"] = config.get('MQTT', 'protocol')
     ret["queueCapacity"] = int(config.get('Calculator', 'queueCapacity'))
     ret["chkTimer"]      = int(config.get('Calculator', 'chkTimer'))
     ret["threshold"]     = int(config.get('Calculator', 'threshold'))
@@ -56,12 +83,16 @@ def init():
 
 if __name__ == '__main__':
     conf = init()
-    calculator = Calculator(conf["queueCapacity"], conf["chkTimer"], conf["threshold"])
+    calculator = Calculator(conf["queueCapacity"],
+                            conf["chkTimer"],
+                            conf["threshold"])
     clnt = initMQTT(conf["url"], conf["port"], conf["keepalive"])
     while True:
         time.sleep(conf["sleepInterval"])
         ret, val = calculator.nearest()
         if ret:
-            clnt.publish(conf["nearest_id"], str('{"id":"%s","val":"%s"}' % (ret, val)))
-            if DEBUG: print(ret, val)
-        
+            clnt.publish(conf["nearest_id"],
+                         str('{"id":"%s","val":"%s"}' % (ret, val)))
+            if DEBUG:
+                print(ret, val)
+
