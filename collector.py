06fc27d55a96bf3005eb51324e0e55e79fbede80
Olimpiu Rob <olimpiu.rob@gmail.com>
2016-05-23 23:13:21 +0300

    Added more MQTT options for collector

--- collector.py
+++ collector.py
@@ -8,14 +8,33 @@ from proximity import *
 
 DEBUG = True
 
+
 def onConnect(client, userdata, rc):
     """MQTT onConnect handler"""
     print("Connected to broker: " + str(rc))
 
-def initMQTT(url = "localhost", port = 1883, keepalive = 60):
+
+def initMQTT(url="localhost", port=1883, keepalive=60, client_id=None,
+             username=None, password=None, certificate=None, client_key=None,
+             client_cert=None, mqtt_protocol="3.1.1"):
     """Init MQTT connection"""
-    client = mqtt.Client()
+    proto = mqtt.MQTTv311
+    if protocol == "3.1":
+        proto = mqtt.MQTTv31
+
+    if not client_id:
+        client = mqtt.Client(protocol=proto)
+    else:
+        client = mqtt.Client(client_id, protocol=proto)
+
+    if username is not None:
+        client.username_pw_set(username, password)
+
+    if certificate is not None:
+        client.tls_set(certificate, certfile=client_cert, keyfile=client_key)
+
     client.on_connect = onConnect
+    client.on_message = onMessage
     try:
         client.connect(url, port, keepalive)
         client.loop_start()
@@ -23,6 +42,7 @@ def initMQTT(url = "localhost", port = 1883, keepalive = 60):
     except Exception, e:
         print(e)
         return None
+
 
 def startScan(mqttclnt, filter="", topic="/ble/rssi/"):
     """Scan BLE beacon and publish to MQTT broker"""
@@ -33,7 +53,9 @@ def startScan(mqttclnt, filter="", topic="/ble/rssi/"):
                 fields = beacon.split(",")
                 if fields[1].startswith(filter):
                     mqttclnt.publish(topic, '{"id":"%s","val":"%s"}' % (fields[0], fields[5]))
-                    if DEBUG: print(fields[0], fields[5])
+                    if DEBUG:
+                        print(fields[0], fields[5])
+
 
 def init():
     """Read config file"""
@@ -42,11 +64,18 @@ def init():
     config.read("config")
     global DEBUG
     DEBUG = True if int(config.get('Collector', 'debug')) == 1 else False
-    ret["url"]       = config.get('MQTT', 'url')
-    ret["port"]      = int(config.get('MQTT', 'port'))
-    ret["keepalive"] = int(config.get('MQTT', 'keepalive'))
-    ret["filter"]    = config.get('Scanner', 'filter')
-    ret["topic_id"]  = config.get('Scanner', 'topic_id')
+    ret["url"]           = config.get('MQTT', 'url')
+    ret["port"]          = int(config.get('MQTT', 'port'))
+    ret["keepalive"]     = int(config.get('MQTT', 'keepalive'))
+    ret["client_id"]     = config.get('MQTT', 'client_id')
+    ret["username"]      = config.get('MQTT', 'username')
+    ret["password"]      = config.get('MQTT', 'password')
+    ret["certificate"]   = config.get('MQTT', 'certificate')
+    ret["client_key"]    = config.get('MQTT', 'client_key')
+    ret["client_cert"]   = config.get('MQTT', 'client_cert')
+    ret["mqtt_protocol"] = config.get('MQTT', 'protocol')
+    ret["filter"]        = config.get('Scanner', 'filter')
+    ret["topic_id"]      = config.get('Scanner', 'topic_id')
     return ret
 
 if __name__ == '__main__':
